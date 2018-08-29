38d31b40309885d832eb4482c720a6f6cb090748
Radek Cehak <cehak@pixelfield.cz>
2018-08-29 10:16:16 +0200

    first commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,15 +1,14 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build ---prod` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
-
 export const environment = {
-  production: false
+	production: false,
+	firebase: {
+		apiKey: 'AIzaSyAfOtZfakHrJqR9zxerJiKgl83I8diP7s4',
+		authDomain: 'fallout-76.firebaseapp.com',
+		databaseURL: 'https://fallout-76.firebaseio.com',
+		projectId: 'fallout-76',
+		storageBucket: 'fallout-76.appspot.com',
+		messagingSenderId: '179583387300'
+	}
 };
 
-/*
- * In development mode, to ignore zone related error stack frames such as
- * `zone.run`, `zoneDelegate.invokeTask` for easier debugging, you can
- * import the following file, but please comment it out in production mode
- * because it will have performance impact when throw error
- */
-// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
+
+
