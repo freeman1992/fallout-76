7159afbd028e0473b03621650b37fbb9921bf9ab
Radek Cehak <cehak@pixelfield.cz>
2018-08-29 11:37:11 +0200

    .htAccess

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,14 @@
 export const environment = {
-  production: true
+	production: true,
+	firebase: {
+		apiKey: 'AIzaSyAfOtZfakHrJqR9zxerJiKgl83I8diP7s4',
+		authDomain: 'fallout-76.firebaseapp.com',
+		databaseURL: 'https://fallout-76.firebaseio.com',
+		projectId: 'fallout-76',
+		storageBucket: 'fallout-76.appspot.com',
+		messagingSenderId: '179583387300'
+	}
 };
+
+
+
